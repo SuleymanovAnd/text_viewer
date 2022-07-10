c4877e880dbb8cdc5c6955236458fff56ec4e244
Suleymanov Andrey <sasus37@gmail.com>
2022-07-10 17:11:23 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
-
+#include <fstream>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    std::cout << "Enter file path:" << std::endl;
+    std::string path;
+    std::getline(std::cin,path)  ;
+
+    std::ifstream file;
+    file.open (path,std::ios::binary);
+    // проверка открылся ли файл
+    while  ( !file.is_open ()){
+        std::cout << "Enter correct file path:" << std::endl;
+        std::cin >> path;
+        file.open (path,std::ios::binary);
+    }
+    //
+    char buffer [100];
+    int size = 100;
+    while (!file.eof()){
+        if (size > 0){
+            file.read (buffer, size);
+            size = (int)file.gcount ();
+        } else {break;}
+
+        for( int i =0; i<size;i++){ // вывод
+            std::cout << buffer [i];
+        }
+    }
+    file.close();
 }
